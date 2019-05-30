c9cb6c916c0953c40b19ea6476b0106a300ef94b
Adrian Messado <adrian.messado@gmail.com>
2019-05-29 21:09:45 -0700

    move Vehicle instances and vehicle array outside of try catch block

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,20 +5,20 @@ import java.io.IOException;
 
 public class App {
     public static void main(String[] args) {
+
+    Vehicle hatchback = new Vehicle(1994, "Subaru", "Legacy", 170000, 4000);
+    Vehicle suv = new Vehicle(2002, "Ford", "Explorer", 100000,7000);
+    Vehicle sedan = new Vehicle(2015, "Toyota", "Camry", 50000, 30000);
+    Vehicle truck = new Vehicle(1999, "Ford", "Ranger", 100000, 4000);
+
+    Vehicle[] allVehicles = {hatchback, suv, sedan, truck};
+
     BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
     System.out.println("What is your maximum budget for a vehicle?");
 
     try {
         String stringUserMaxBudget = bufferedReader.readLine();
 
-
-        Vehicle hatchback = new Vehicle(1994, "Subaru", "Legacy", 170000, 4000);
-        Vehicle suv = new Vehicle(2002, "Ford", "Explorer", 100000,7000);
-        Vehicle sedan = new Vehicle(2015, "Toyota", "Camry", 50000, 30000);
-        Vehicle truck = new Vehicle(1999, "Ford", "Ranger", 100000, 4000);
-
-
-        Vehicle[] allVehicles = {hatchback, suv, sedan, truck};
 
         int userMaxBudget = Integer.parseInt(stringUserMaxBudget);
 
